fd26a8b374afc56192da6f844d1bebc087b1357f
ListKelvin <phamm5687@gmai.com>
2022-06-12 18:17:24 +0700

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import  {useState, useEffect} from'react';
 import './App.css';
 
 function App() {
+  const [work, setwork] = useState('');
+  const [listWorks, setListWorks] = useState(
+    ()=>{
+      const storageWork = JSON.parse(localStorage.getItem('works'))
+      return storageWork ;
+    }
+  );
+  // console.log(listWorks);
+  const handleAddWork=() =>{
+    if(!work){
+      alert('bạn chưa nhập gì !!');
+      return ;
+    }
+      setListWorks( prev => {
+
+        const newWorks = [...prev, work]
+          // save local
+        const jsonWorks = JSON.stringify(newWorks)
+        localStorage.setItem('works',jsonWorks);
+        return newWorks;
+      })
+      setwork('');
+  }
+  /////////////////////////
+  const handleDelete =(todo) =>{
+    let currentWork = listWorks;
+      currentWork = currentWork.filter(item => item !==  todo);
+      setListWorks(currentWork);
+      
+  }
+  useEffect(() => {
+    const todo = localStorage.getItem('works');
+    if(todo !== null) setListWorks(JSON.parse(todo))
+
+},[])
+//////////////////////////////////////////////////////////
+  useEffect(()=>{
+    const updateWork = JSON.stringify(listWorks);
+    localStorage.setItem('works',updateWork)
+  }, [listWorks])
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>To Do lisst</p>
+        <input className='input' value={work}  onChange={(event) => setwork(event.target.value)}/>
+        <button className='btn' onClick={handleAddWork} >ADD</button>
+
+        <ul >
+          {listWorks.map((item,index)=>(
+              <li key={index}> {index + 1} - {item}  <button className='btn' onClick={()=> handleDelete(item)}>DELETE</button> </li> 
+          ))}  
+        </ul>
     </div>
   );
 }
